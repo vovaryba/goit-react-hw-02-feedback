e29b732bab85c3f1ab5c42aa9112b2dcfd872434
Volodymyr Ryba <vovaryba@gmail.com>
2021-07-14 23:05:54 +0300

    Сделал компонент статистики, но пока не подключаю

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+// import s from './Statistics.module.css';
+
+class Statistics extends Component {
+  //   state = {
+  //     good: 0,
+  //     neutral: 0,
+  //     bad: 0,
+  //   };
+
+  render() {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <p>Good:</p>
+        <p>Neutral:</p>
+        <p>Bad:</p>
+      </div>
+    );
+  }
+}
+
+export default Statistics;
