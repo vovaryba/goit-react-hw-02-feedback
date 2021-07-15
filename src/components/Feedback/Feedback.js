e881a82521719c0ae213bbe4ccb9f2a9d61a2778
Volodymyr Ryba <vovaryba@gmail.com>
2021-07-16 00:10:16 +0300

    Сделал шаг 2 первого задания

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import s from './Feedback.module.css';
+import s from './Feedback.module.css';
 
 class Feedback extends Component {
   state = {
@@ -9,38 +9,62 @@ class Feedback extends Component {
   };
 
   addGoodRev = () => {
-    this.setState(prevState => {
-      return { good: prevState.good + 1 };
-    });
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
   };
   addNeutralRev = () => {
-    this.setState(prevState => {
-      return { neutral: prevState.neutral + 1 };
-    });
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
   };
   addBadRev = () => {
-    this.setState(prevState => {
-      return { bad: prevState.bad + 1 };
-    });
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
   };
 
   render() {
+    const total = this.countTotalFeedback();
+    const positiveFeedback = this.countPositiveFeedbackPercentage();
     return (
       <div>
         <h2>Please leave feedback</h2>
-        <button type="button" onClick={this.addGoodRev}>
+        <button
+          className={s.feedbackButton}
+          type="button"
+          onClick={this.addGoodRev}
+        >
           Good
         </button>
-        <button type="button" onClick={this.addNeutralRev}>
+        <button
+          className={s.feedbackButton}
+          type="button"
+          onClick={this.addNeutralRev}
+        >
           Neutral
         </button>
-        <button type="button" onClick={this.addBadRev}>
+        <button
+          className={s.feedbackButton}
+          type="button"
+          onClick={this.addBadRev}
+        >
           Bad
         </button>
         <h2>Statistics</h2>
-        <p>Good: {this.state.good}</p>
-        <p>Neutral: {this.state.neutral}</p>
-        <p>Bad: {this.state.bad}</p>
+        <p className={s.feedbackCounter}>Good: {this.state.good}</p>
+        <p className={s.feedbackCounter}>Neutral: {this.state.neutral}</p>
+        <p className={s.feedbackCounter}>Bad: {this.state.bad}</p>
+        <p className={s.feedbackCounter}>Total: {total}</p>
+        <p className={s.feedbackCounter}>
+          Positive feedback: {this.state.good === 0 ? 0 : positiveFeedback}%
+        </p>
       </div>
     );
   }
