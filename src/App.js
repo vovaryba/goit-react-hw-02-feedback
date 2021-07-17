9899c6e0b4a4c58186fc800da230c3d6532afa3e
Volodymyr Ryba <vovaryba@gmail.com>
2021-07-17 20:44:46 +0300

    Основной рендер страницы, состояние и методы - все перенес в App из Feedback

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
-import React from 'react';
-import Feedback from './components/Feedback';
-// import Statistics from './components/Statistics';
+import React, { Component } from 'react';
+import Statistics from './components/Statistics';
+import FeedbackOptions from './components/FeedbackOptions';
+import Section from './components/Section';
 
-export default function App() {
-  return (
-    <div>
-      <Feedback />
-      {/* <Statistics /> */}
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  addGoodRev = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  addNeutralRev = () => {
+    this.setState(prevState => ({
+      neutral: prevState.neutral + 1,
+    }));
+  };
+  addBadRev = () => {
+    this.setState(prevState => ({
+      bad: prevState.bad + 1,
+    }));
+  };
+  countTotalFeedback = () => {
+    const { good } = this.state;
+    const { neutral } = this.state;
+    const { bad } = this.state;
+    return good + neutral + bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good } = this.state;
+    const { neutral } = this.state;
+    const { bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={{
+              addGood: this.addGoodRev,
+              addNeutral: this.addNeutralRev,
+              addBad: this.addBadRev,
+            }}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positiveFeedbackPercentage}
+          />
+        </Section>
+      </>
+    );
+  }
 }
+
+export default App;
