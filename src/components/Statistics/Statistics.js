97e2cadb23ba461b42f6436ad91553b3b5518d08
Volodymyr Ryba <vovaryba@gmail.com>
2021-07-17 20:40:35 +0300

    Рендер статистики по условию

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,23 +1,34 @@
-import React, { Component } from 'react';
-// import s from './Statistics.module.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+import Notification from '../Notification';
 
-class Statistics extends Component {
-  //   state = {
-  //     good: 0,
-  //     neutral: 0,
-  //     bad: 0,
-  //   };
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  const { feedbackCounter } = s;
+  return (
+    <>
+      {total > 0 && (
+        <>
+          <p className={feedbackCounter}>Good: {good}</p>
+          <p className={feedbackCounter}>Neutral: {neutral}</p>
+          <p className={feedbackCounter}>Bad: {bad}</p>
+          <p className={feedbackCounter}>Total: {total}</p>
+          <p className={feedbackCounter}>
+            Positive feedback: {good === 0 ? 0 : positivePercentage}%
+          </p>
+        </>
+      )}
+      {total === 0 && <Notification message="No feedback given" />}
+    </>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <h2>Statistics</h2>
-        <p>Good:</p>
-        <p>Neutral:</p>
-        <p>Bad:</p>
-      </div>
-    );
-  }
-}
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
 
 export default Statistics;
