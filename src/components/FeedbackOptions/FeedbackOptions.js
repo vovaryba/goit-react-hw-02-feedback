7a5d124f171bb7f782a017493f7e9532f8d254fe
Volodymyr Ryba <vovaryba@gmail.com>
2021-07-17 22:30:07 +0300

    Переделал рендер кнопок

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -2,28 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './FeedbackOptions.module.css';
 
-const FeedbackOptions = ({ options }) => {
-  const { addGood } = options;
-  const { addNeutral } = options;
-  const { addBad } = options;
+const FeedbackButton = ({ feedback, onLeaveFeedback }) => {
   const { feedbackButton } = s;
   return (
-    <>
-      <button className={feedbackButton} type="button" onClick={addGood}>
-        Good
-      </button>
-      <button className={feedbackButton} type="button" onClick={addNeutral}>
-        Neutral
-      </button>
-      <button className={feedbackButton} type="button" onClick={addBad}>
-        Bad
-      </button>
-    </>
+    <button
+      key={feedback}
+      className={feedbackButton}
+      data-feedback={feedback}
+      type="button"
+      onClick={onLeaveFeedback}
+    >
+      {feedback}
+    </button>
+  );
+};
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return options.map(option =>
+    FeedbackButton({ feedback: option, onLeaveFeedback }),
   );
 };
 
 FeedbackOptions.propTypes = {
-  options: PropTypes.object.isRequired,
+  options: PropTypes.arrayOf(PropTypes.string),
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
 
 export default FeedbackOptions;
